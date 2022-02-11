e8e25a854269903a2aa7ef0468dec779712c9cd8
bilalobaidat <bilal_obaidat@yahoo.com>
2022-02-11 11:42:55 -0700

    Add files via upload

---
+++ Pages/Control_Pannel.php
@@ -0,0 +1,20 @@
+<div class="Calendar">
+    <div class="Weeks" class="center">
+        <button id="W1" class="button W1">Week 1</button>
+        <button id="W2" class="button W2">Week 2</button>
+        <button id="W3" class="button W3">Week 3</button>
+        <button id="W4" class="button W4">Week 4</button>
+    </div>
+
+    <div class="Days" class="center">
+        <button id="Mon" class="button Mon">Monday</button>
+        <button id="Tue" class="button Tue">Tuesday</button>
+        <button id="Wed" class="button Wed">Wednesday</button>
+        <button id="Thu" class="button Thu">Thursday</button>
+        <button id="Fri" class="button Fri">Friday</button>
+        <button id="Sat" class="button Sat">Saturday</button>
+        <button id="Sun" class="button Sun">Sunday</button>
+    </div>
+</div>
+
+<hr>
